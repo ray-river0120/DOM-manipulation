24c98dcd90196814d1b9dd890994624ab1c907ab
Nhat Vu <nmv0120@gmail.com>
2022-09-28 19:52:48 -0400

    1st commit

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // Do your magic here!
- 
+function changeBackgroundColor() {
+    document.getElementById("container").style.backgroundColor = "beige";
+}
 
 
+function validate() {
+    if (document.getElementById("numb").value < 1 || document.getElementById("numb").value > 10) {
+        document.getElementById('demo').innerHTML = "Invalid input";
+    } else {
+        document.getElementById('demo').innerHTML = "Valid input";
+    }
+}
+
+function checkDateTime() {
+    document.getElementById('myBtn').innerHTML = alert(Date());
+}
