67f997fcb00a2a1f761912e6d4bbabd9700c368e
Nhat Vu <nmv0120@gmail.com>
2022-09-28 20:53:51 -0400

    next commit

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 // Do your magic here!
-function changeBackgroundColor() {
-    document.getElementById("container").style.backgroundColor = "beige";
-}
+let colors = ["blue", "yellow", "dark", "red", "brown", "orang"];
+//get button
+let button = document.getElementById("button");
+//add event listener
+button.addEventListener("click", function () {
+    //randomizer
+
+    let randomColors = colors[Math.floor(Math.random() * colors.length)];
+    //get container
+    let container = document.getElementById("container");
+
+    container.style.background = randomColors;
+});
+
 
 
 function validate() {
-    if (document.getElementById("numb").value < 1 || document.getElementById("numb").value > 10) {
-        document.getElementById('demo').innerHTML = "Invalid input";
+    let x = document.getElementById('numb').value;
+    if (isNaN(x) || x < 1 || x > 10) {
+        document.getElementById('demo').innerHTML = "Input not valid";
+        return false;
     } else {
-        document.getElementById('demo').innerHTML = "Valid input";
+        document.getElementById('demo').innerHTML = "Input Valid";
+        return true;
     }
 }
 
